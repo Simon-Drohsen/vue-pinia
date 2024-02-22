de706b17b113616e5fad755492328e06e022651c
Simon-Drohsen <simondrohsend@gmail.com>
2024-02-22 16:27:18 +0100

    video 9/14 done

---
+++ src/stores/CartStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia";
+export const useCartStore = defineStore("CartStore", {
+    state: () => {
+        return {
+            items: []
+        };
+    },
+    getters: {
+        count: (state) => state.items.length,
+        isEmpty: (state) => state.count === 0
+    },
+    actions: {
+        addItems(count, item) {
+            count = parseInt(count);
+            for (let i = 0; i < count; i++) {
+                this.items.push({ ...item });
+            }
+        },
+    }
+})
